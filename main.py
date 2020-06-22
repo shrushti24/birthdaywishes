e83f66e346d901ebfd4487d1b2915e537649f343
Priyanshu Asthana <62474294+asthana001@users.noreply.github.com>
2020-06-22 09:53:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ import pandas as pd
 import datetime
 import smtplib
 import getpass
-#from PIL import Image
 
-#GMAIL_ID = "useasspam1@gmail.com"
-#GMAIL_PASSWORD = "priyanshu123"
-GMAIL_ID = input("Enter your email id: ")
+GMAIL_ID = input("Enter your Gmail id: ")
 GMAIL_PASSWORD = getpass.getpass(prompt="Enter your password: ")
 
 
 def send_email(to, sub, msg):
-    # print(f"Email sent to {to} with title {sub} and message : {msg}")
-    #img = Image.open("birthday-wishes-facebook-friend.jpg")
+    # use of smtp to send emails through port 587. For more information visit [Gmail SMTP Docs](https://support.google.com/a/answer/176600?hl=en)
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
     s.login(GMAIL_ID, GMAIL_PASSWORD)
@@ -23,16 +19,10 @@ def send_email(to, sub, msg):
 
 if __name__ == "__main__":
     df = pd.read_excel("data.xlsx")
-    # print(df)
     today = datetime.datetime.now().strftime("%d-%m")
-    # print(today)
 
-    # img = Image.open("birthday-wishes-facebook-friend.jpg")
-    # img.show()
 
     # iterating through the data sheet file row by row
     for index, item in df.iterrows():
-        # print(item["Birthday"].strftime("%d-%m"))
-        if item["Birthday"].strftime("%d-%m") == today:
-            # print(item["Name"], "Yes")
+        if item["Birthday"].strftime("%d-%m") == today
             send_email(item["Email"], "Birthday Greetings", item["Message"])
